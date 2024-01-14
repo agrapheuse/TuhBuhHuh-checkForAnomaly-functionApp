58c993008bec313b69c048c22e8228d13b0527c2
Paul Ashioya <John.ashioya@gmail.com>
2024-01-14 18:20:58 +0100

    fixed version

--- function_app.py
+++ function_app.py
@@ -5,7 +5,7 @@ import os
 from io import StringIO
 from azure.storage.blob import BlobServiceClient
 import pandas as pd
-from sklearn.impute import KNNImputer
+from sklearn.impute import KNNImputer, SimpleImputer
 from sklearn.ensemble import IsolationForest
 import numpy as np
 
@@ -21,9 +21,9 @@ def main(message: func.ServiceBusMessage) -> None:
                 message.get_body().decode('utf-8'))
     
     file_path = message.get_body().decode('utf-8')
-    
     # remove the first '/history' from the path
     file_path = file_path.replace("/history", "", 1)
+    square_uuid = file_path.split('/')[1]
     
     connection_string = "DefaultEndpointsProtocol=https;AccountName=datalaketuhbehhuh;AccountKey=C2te9RgBRHhIH8u3tydAsn9wNd4umdD2axq1ZdcfKh7CZRpL04+D4H6QinE/gckMTUA/dFj1kFpd+ASt4+/8ZA==;EndpointSuffix=core.windows.net"
     blob_service_client = BlobServiceClient.from_connection_string(connection_string)
@@ -35,11 +35,15 @@ def main(message: func.ServiceBusMessage) -> None:
     data = download_blob_to_file(blob_service_client, 'csv', file_path) 
     logging.warning('Blob downloaded: %s', file_path)
     
-    original_data = data.copy()
-    structured_data = process_data(data, original_data)
-    anomalies = detect_anomaly(structured_data,original_data)
-    logging.warning('Anomalies detected: %s', anomalies)
-    send_anomaly_data_to_queue(anomalies)
+    cleaned_data = process_data(data)
+    anomalies = detect_anomaly(cleaned_data)
+    
+    if anomalies.empty == False:
+        logging.warning('Anomalies detected: %s', anomalies)
+        anomaly_df = process_anomalies(anomalies, square_uuid=square_uuid)
+        send_anomaly_data_to_queue(anomaly_df)
+    else:
+        logging.warning('No anomalies detected')
 
 def download_blob_to_file(blob_service_client: BlobServiceClient, container_name, blob_name):
     blob_client = blob_service_client.get_blob_client(container=container_name, blob=blob_name)
@@ -49,15 +53,36 @@ def download_blob_to_file(blob_service_client: BlobServiceClient, container_name
     return df
 
 
+def process_anomalies(anomalies, square_uuid):
+    anomaly_df = pd.DataFrame(columns=['location', 'dateTime', 'type', 'value'])
+
+    # Create a list to store DataFrames for each anomaly
+    anomaly_frames = []
+
+    # Iterate over each timestamp/location and create a DataFrame for each anomaly
+    for idx, row in anomalies.iterrows():
+        anomaly_frames.append(pd.DataFrame({
+            'location': [square_uuid] * len(row),
+            'dateTime': [idx] * len(row),
+            'type': row.index,
+            'value': row.values
+        }))
+
+    # Concatenate the list of DataFrames into a single DataFrame
+    anomaly_df = pd.concat(anomaly_frames, ignore_index=True)
+    
+    logging.warning('Anomalies Processed: %s', anomaly_df)
+    return anomaly_df
+
 
 def send_anomaly_data_to_queue(anomalies):
     connection_params = pika.ConnectionParameters(
-        host=os.environ['RABBITMQ_HOST'],
-        port=os.environ['RABBITMQ_PORT'],
-        virtual_host=['RABBITMQ_VHOST'],
-        credentials=pika.PlainCredentials(os.environ['RABBITMQ_USER'], os.environ['RABBITMQ_PASS'])
+        host=os.environ['RABBIT_MQ_HOST'],
+        port=os.environ['RABBIT_MQ_PORT'],
+        virtual_host=os.environ['RABBIT_MQ_VHOST'],
+        credentials=pika.PlainCredentials(os.environ['RABBIT_MQ_USER'], os.environ['RABBIT_MQ_PASS'])
     )
-    queue_name = "new_anomaly_event"
+    queue_name = "new_anomaly_queue"
 
     connection = pika.BlockingConnection(connection_params)
     channel = connection.channel()
@@ -67,37 +92,30 @@ def send_anomaly_data_to_queue(anomalies):
 
     connection.close()
 
-def process_data(df, original_data):
+def process_data(df):
     logging.warning('Processing data: %s', df)
     # if there is a column with no values at all, it will be dropped
     df = df.dropna(axis=1, how='all')
     
     #  Using KnnImputer to fill the missing values
-    kni = KNNImputer(missing_values=np.nan, copy=False, add_indicator=True, weights='distance')
-    df = pd.DataFrame(df=kni.fit_transform(df), index=original_data.index)
-    
-    # Put the deleted/empty columns back.
-    original_data = original_data.drop(df.columns, axis=1)
-    df = pd.concat([df, original_data], axis=1)
-    
+    # kni = KNNImputer(missing_values=np.nan, copy=False, add_indicator=True, weights='distance')
+    kni = SimpleImputer(missing_values=np.nan, strategy='mean')
+    df = pd.DataFrame(kni.fit_transform(df), index=df.index, columns=df.columns)    
     logging.warning('Data processed: %s', df)
     return df
 
 
-def detect_anomaly(data, original_data):
-    logging.warning('Detecting anomalies')
-    # if there is a column with no values at all, it will be dropped
-    data = data.dropna(axis=1, how='all')
-    model=IsolationForest(n_estimators=100,max_samples='auto',contamination=float(0.2),random_state=42)
+def detect_anomaly(data):
+    logging.warning('Detecting anomalies')    
+    model=IsolationForest(n_estimators=100,max_samples='auto',random_state=42)
     model.fit(data)
     y_pred_outliers = model.predict(data)
-    data['anomaly_score'] = model.decision_function(data)
     data['outlier'] = y_pred_outliers
     
-    # Put the deleted/empty columns back.
-    original_data = original_data.drop(data.columns, axis=1)
-    data = pd.concat([original_data, data], axis=1)  
-    
-    # only return rows where  outlier is -1
-    data = data[data['outlier']==-1]
-    return data
+    # Make sure 'outlier' is a column before filtering
+    if 'outlier' in data.columns:
+        # anomaly_columns = data.columns[data['outlier'] == -1].tolist()  # Convert to list
+        return data
+    else:
+        logging.warning("No 'outlier' column found in the DataFrame.")
+        return pd.DataFrame()  # Return an empty DataFrame if 'outlier' column is missing
